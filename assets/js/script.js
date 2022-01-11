328cd3fcec8785fd32d26f03b5a12cd723546885
5ahexn <5ahexn@gmail.com>
2022-01-11 21:25:47 +0900

    Update
    Popup Page

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,5 +28,74 @@ $(document).ready(function(){
         autoplay: true,
         autoplaySpeed: 3000,
         dots: true
-      });
+    });
+
+    // 탭메뉴 (+ tab버튼을 눌렀을 때 웹표준을 준수하기 위한 스크립트)
+    // 변수 = 데이터 저장소
+    let $tab_list = $(".tab_menu");
+
+    $tab_list.find("ul ul").hide();
+    $tab_list.find("li.active > ul").show();
+
+    function tabMenu(e) {
+        e.preventDefault();
+        let $this = $(this);
+        $this.next("ul").show().parent("li").addClass("active").siblings("li").removeClass("active").find(">ul").hide();
+    }
+
+    $tab_list.find("ul>li>a").click(tabMenu).focus(tabMenu);
+
+    // 갤러리
+    $(".gallery_img").slick({
+        arrows: false,
+        fade: true,
+        pauseOnHover: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 150,
+        slidesToShow: 1
+    });
+
+    $(".paly").click(function(){
+        $(".gallery_img").slick("slickPlay");
+    });
+
+    $(".pause").click(function(){
+        $(".gallery_img").slick("slickPause");
+    });
+
+    $(".prev").click(function(){
+        $(".gallery_img").slick("slickPrev");
+    });
+
+    $(".next").click(function(){
+        $(".gallery_img").slick("slickNext");
+    });
+
+    // 레이어 팝업
+    $(".layer").click(function(e){
+        e.preventDefault();
+        // $("#layer").css("display","block");
+        // $("#layer").show();
+        // $("#layer").fadeIn();
+        $("#layer").slideDown();
+    });
+
+    $("#layer .close").click(function(e){
+        e.preventDefault();
+        // $("#layer").css("display","none");
+        $("#layer").hide();
+        // $("#layer").fadeOut();
+        // $("#layer").slideUp();
+    });
+
+    // 윈도우 팝업
+    $(".window").click(function(e){
+        e.preventDefault();
+        // window.open("파일명", "팝업이름", "옵션설정")
+        // 옵션 : top, left, width, status, toolbar, location, menubar, scrollbars, fullscreen 사용가능
+        
+        window.open("popup.html","popup01", "width=800, height=590, top=50, left=50, scrollbars=0, toolbar=0, menubar=0");
+    });
 })
